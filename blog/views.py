67018af67e589e0b789c4278d8b78fe5613c7010
HellooYing <15642330153@163.com>
2017-10-25 00:47:45 +0800

    终于让views能够给出我需要的字典了qaq

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,25 @@
 from django.urls import reverse
 from blog.models import Blog
-from django.shortcuts import render, redirect,render_to_response
+from django.shortcuts import render, redirect, render_to_response
 import os
+import json
+
 
 def index(request):
-    filelist=os.listdir('c:/code/myblog/blog/static/picture')#该文件夹下所有的文件（包括文件夹）
-    for files in filelist:#遍历所有文件
-        Olddir=os.path.join('c:/code/myblog/blog/static/picture',files);#原来的文件路径
-        filename=os.path.splitext(files)[0];#文件名
-        filetype=os.path.splitext(files)[1];#文件扩展名
-        Newdir=os.path.join('c:/code/myblog/blog/static/picture',filename+".jpg");#新的文件路径
-        os.rename(Olddir,Newdir);#重命名
-    a=Blog.objects.all()
-    return render(request,'index.html',context={"all_blog":a})
+    filelist = os.listdir('c:/code/myblog/blog/static/picture')
+    for files in filelist:
+        Olddir = os.path.join('c:/code/myblog/blog/static/picture', files)
+        filename = os.path.splitext(files)[0]
+        filetype = os.path.splitext(files)[1]
+        Newdir = os.path.join(
+            'c:/code/myblog/blog/static/picture', filename + ".jpg")
+        os.rename(Olddir, Newdir)
+    a = Blog.objects.all()
+    dict1={}
+    for i in a:
+        dict1['title'+str(i.id)]=i.title
+        dict1['brief'+str(i.id)]=i.brief
+        dict1['time'+str(i.id)]=i.time
+        dict1['picture'+str(i.id)]=i.picture
+        dict1['article'+str(i.id)]=i.article
+    return HttpResponse(json.dumps(dict1), content_type="application/json")
