4bcb09e968d0d169c9035cd1f42074d5ded60026
HellooYing <15642330153@163.com>
2017-10-27 00:04:57 +0800

    除了'美观'以外的要求都做完了...

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
-urlpatterns = [
+from django.views.generic.base import RedirectView
 
+urlpatterns = [
+    
     url(r'^$',views.index,name='index'),
     url(r'^index_waterfall/',views.index_waterfall,name='index_waterfall'),
+    url(r'^favicon.ico$',RedirectView.as_view(url=r'static/favicon.ico')),
 ]
