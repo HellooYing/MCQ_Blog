f1a917a391f91e0a8f1d730272ae878343871d64
hellooying <15642330153@163.com>
2018-10-08 15:47:03 +0800

    单独移动端

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     
     url(r'^$',views.index,name='index'),
+    url(r'^phone$',views.phone,name='phone'),
     url(r'^index_waterfall/',views.index_waterfall,name='index_waterfall'),
     url(r'^favicon.ico$',RedirectView.as_view(url=r'static/favicon.ico')),
     url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
